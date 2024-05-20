36c803462b28f318fa806ec818a93e1a2220c88d
raoufffffff <112146388+raoufffffff@users.noreply.github.com>
2024-05-20 16:18:21 +0100

    Add files via upload

---
+++ models/order.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const order = new mongoose.Schema({
+    name: String,
+    price: Number,
+    orders: [],
+    method: String,
+    active: Boolean,
+    phone: String,
+    address: String
+})
+
+
+const Order = mongoose.model('order', order)
+
+module.exports = Order
